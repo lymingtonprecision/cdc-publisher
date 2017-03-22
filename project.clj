b2b55d100e466ff9bd659c1d5ca94a97d88ed5a4
Adam Harper <github@adam-harper.com>
2017-03-22 08:23:33 +0000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -5,25 +5,25 @@
   :url "https://github.com/lymingtonprecision/cdc-publisher"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.442"]
 
                  ;; system
-                 [com.stuartsierra/component "0.3.1"]
-                 [environ "1.0.1"]
-                 [clj-time "0.11.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [environ "1.1.0"]
+                 [clj-time "0.13.0"]
 
                  ;; logging
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-logging-config "1.9.12"]
 
                  ;; metrics
-                 [metrics-clojure "2.6.0"]
+                 [metrics-clojure "2.9.0"]
                  [metrics-kafka-reporter/reporter-clj "0.1.0"]
 
                  ;; database
-                 [org.clojure/java.jdbc "0.4.2"]
-                 [yesql "0.5.1"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [yesql "0.5.3"]
 
                  ;; oracle jars
                  [oracle/ojdbc6 ~oracle-jar-version]
@@ -34,15 +34,15 @@
                  [oracle/xmlparserv2 ~oracle-jar-version]
 
                  ;; (de-)serialization
-                 [cheshire "5.5.0"]
+                 [cheshire "5.7.0"]
 
                  ;; kafka
-                 [org.apache.kafka/kafka-clients "0.9.0.0"]
+                 [org.apache.kafka/kafka-clients "0.10.2.0"]
                  [clj-kafka "0.3.4"]
                  [org.clojars.ah45/clj-kafka-util "0.1.1"]
 
                  ;; utils
-                 [lymingtonprecision/cdc-util "1.2.0"]]
+                 [lymingtonprecision/cdc-util "1.2.2"]]
 
   :main cdc-publisher.main
   :aot [cdc-publisher.main]
@@ -52,9 +52,9 @@
   :plugins [[lein-localrepo "0.5.3"]]
 
   :profiles {:repl {:source-paths ["dev"]}
-             :dev {:dependencies [[reloaded.repl "0.2.1"]
+             :dev {:dependencies [[reloaded.repl "0.2.3"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [com.gfredericks/test.chuck "0.2.5"]]}}
+                                  [com.gfredericks/test.chuck "0.2.7"]]}}
 
   :repl-options {:init-ns user :init (reloaded.repl/init)}
 
